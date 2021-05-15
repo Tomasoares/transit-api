c684a064f1aa0798f9d7fa90d8b007bd7a6264c4
Tomas <tomasoares@gmail.com>
2021-05-15 22:15:04 +0100

    Intial commit

---
+++ src/main/java/com/exercise/transitapi/TransitApiApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.transitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/transitapi/TransitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.transitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
